fc8cde0734385171054d37a1f9afee182bec17c0
Grigoriy <volchonokgrisha@gmail.com>
2020-11-02 13:11:55 +0300

    Add main form with menu.

---
+++ MainMenu.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace dis_lab_2
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DISToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SDISToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.DISToolStripMenuItem,
+            this.SDISToolStripMenuItem,
+            this.AboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.fileToolStripMenuItem.Text = "Файл";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // DISToolStripMenuItem
+            // 
+            this.DISToolStripMenuItem.Name = "DISToolStripMenuItem";
+            this.DISToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.DISToolStripMenuItem.Text = "РИС";
+            // 
+            // SDISToolStripMenuItem
+            // 
+            this.SDISToolStripMenuItem.Name = "SDISToolStripMenuItem";
+            this.SDISToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.SDISToolStripMenuItem.Text = "ЗРИС";
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.AboutToolStripMenuItem.Text = "Об авторе";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStripMain";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(105, 20);
+            this.toolStripStatusLabel1.Text = "Дата и время:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 20);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainMenu";
+            this.Text = "Распределенные Информационные Системы";
+            this.Load += new System.EventHandler(this.MainApp_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DISToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SDISToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+    }
+}
+
---
+++ MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+
+namespace dis_lab_2
+{
+    public partial class MainMenu : Form
+    {
+        
+        public MainMenu()
+        {
+            InitializeComponent();
+
+            Timer DataTimeTimer = new Timer();
+            DataTimeTimer.Interval = 1000;
+
+            // Hook up the Elapsed event for the timer. 
+            DataTimeTimer.Elapsed += UpdateDataTimeTimer;
+
+            // Have the timer fire repeated events (true is the default)
+            DataTimeTimer.AutoReset = true;
+
+            // Start the timer
+            DataTimeTimer.Enabled = true;
+        }
+        private void MainApp_Load(object sender, EventArgs e)
+        {
+            /*
+            DateTime CurrentDateTime = DateTime.Now;
+            toolStripStatusLabel2.Text = CurrentDateTime.ToString();
+            */
+        }
+        private void UpdateDataTimeTimer(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime CurrentDateTime = DateTime.Now;
+            toolStripStatusLabel2.Text = CurrentDateTime.ToString();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
